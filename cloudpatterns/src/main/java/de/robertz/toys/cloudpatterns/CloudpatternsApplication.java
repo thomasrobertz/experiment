5b39555e1f31576bb8127ae0fe3af1219ac2a540
Thomas Robertz <thomas.robertz@googlemail.com>
2022-12-24 14:34:19 +0100

    Cloud Patterns initial commit

---
+++ cloudpatterns/src/main/java/de/robertz/toys/cloudpatterns/CloudpatternsApplication.java
@@ -0,0 +1,13 @@
+package de.robertz.toys.cloudpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudpatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudpatternsApplication.class, args);
+	}
+
+}
---
+++ cloudpatterns/src/test/java/de/robertz/toys/cloudpatterns/CloudpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.robertz.toys.cloudpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
